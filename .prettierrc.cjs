4b1b71407c50ca3bb6f66ed9e028fd57e5eb751f
Ramon Najera <ramonnajera@hotmail.com>
2023-03-26 12:35:09 -0600

    [UPDATE] config

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    useTabs: true,
+    plugins: [
+        ("prettier-plugin-astro")
+    ],
+    overrides: [
+        {
+            files: "**/*astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+};
